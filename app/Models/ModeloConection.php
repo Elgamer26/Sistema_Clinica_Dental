c8effe377851a0bee82a015e7b23efb8ba4252ab
Elgamer26 <jorgemoisesramirez201422@gmail.com>
2025-02-22 20:28:21 -0500

    Docker

--- app/Models/ModeloConection.php
+++ app/Models/ModeloConection.php
@@ -12,14 +12,13 @@ class ModeloConection
     //esto es para la conexion habrir la conexion la base de datos
     public function conexionPDO()
 {
-    $this->host = "localhost";
+    $this->host = "172.17.0.2"; // 172.17.0.2 - IP del Mysql en Docker // 127.0.0.1 Host de Xampp sin Docker
     $this->usuario = "root";
     $this->contraseña = "elgamer1";
     $this->db = "clinicadental";
-    $this->puerto = "3307"; // Agrega el puerto correcto
 
     try {
-        $pdo = new PDO("mysql:host=$this->host;port=$this->puerto;dbname=$this->db", $this->usuario, $this->contraseña);
+        $pdo = new PDO("mysql:host=$this->host;dbname=$this->db", $this->usuario, $this->contraseña);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->exec("set names utf8");
         return $pdo;
